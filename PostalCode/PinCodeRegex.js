3a668e9af3300740e15fefb96fae9d4e301f9b5b
vivek pathak <vivekkpaathak@gmail.com>
2025-03-05 10:46:17 +0530

    Vivek Bhaskar ADD. PIN CODE UC2

--- PostalCode/PinCodeRegex.js
+++ PostalCode/PinCodeRegex.js
@@ -4,5 +4,16 @@
 
 const Regex= /[1-9][0-9]{5}/;
 
-let check= Regex.test("000088");
+let check= Regex.test("500088");
 console.log(check);
+
+
+//UC2
+
+const Regex2= /^[1-9][0-9]{5}/;
+let check1= Regex.test("500088");
+console.log(check1);
+let check2= Regex.test("A00088");
+console.log(check2);
+let check3= Regex.test("A40088");
+console.log(check3);
