eaa0347419f2b6d04be35683ee2758180cca2be6
vivek pathak <vivekkpaathak@gmail.com>
2025-03-05 11:40:45 +0530

    Vivek Bhaskar ADD. Email Regex UC2

--- EmailRegex.js
+++ EmailRegex.js
@@ -1,5 +1,6 @@
 
 //UC1
+console.log("UC1")
 const emailRegex = /^abc(\.\w+)?@bridgelabz\.co(\.\w+)?$/;
 
 let testEmails = [
@@ -17,3 +18,28 @@ let testEmails = [
 testEmails.forEach(email => {
     console.log(`${email}: ${emailRegex.test(email)}`);
 });
+
+
+
+//UC2
+
+console.log("UC2")
+
+const emailRegex2 = /^abc(\.\w+)?@bridgelabz\.co(\.\w+)?$/;
+
+let testEmails2 = [
+    "abc@bridgelabz.co",
+    "abc.xyz@bridgelabz.co",
+    "abc@bridgelabz.co.in",
+    "abc.xyz@bridgelabz.co.in",
+    "xyz@bridgelabz.co",
+    "abc@xyz.co",
+    "abc@bridgelabz.com",
+    "abc.xyz@bridgelabzco.in",
+    "abcxyz@bridgelabz.co"
+];
+
+testEmails2.forEach(email => {
+    console.log(`${email}: ${emailRegex2.test(email)}`);
+});
+
